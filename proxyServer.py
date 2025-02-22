aa0e95bace24faf61f2668e57df204ff1e4b076d
jdbenavente <daniel.benavent3@gmail.com>
2025-02-21 20:28:40 -0700

    Working??

--- proxyServer.py
+++ proxyServer.py
@@ -1,58 +1,66 @@
 from socket import *
 import sys
+import os
 
 if len(sys.argv) <= 1:
-	print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
-	sys.exit(2)
-	
-# The proxy server is listening at 8888 
+    print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server]')
+    sys.exit(2)
+
+# The proxy server is listening at 8888
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind((sys.argv[1], 8888))
 tcpSerSock.listen(100)
 
-while 1:
-	# Strat receiving data from the client
-	print('Ready to serve...')
-	## FILL IN HERE...
+while True:
+    print('Ready to serve...')
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('Received a connection from:', addr)
 
+    message = tcpCliSock.recv(4096).decode()
+    if not message:
+        tcpCliSock.close()
+        continue
 
-	print('Received a connection from:', addr)
+    print(message)
+    filename = message.split()[1][1:]
+    filetouse = "cache/" + filename.replace('/', '_')
+    fileExist = False
 
-	message = ## FILL IN HERE...
-	print(message)
-	# Extract the filename from the given message
+    try:
+        with open(filetouse, "rb") as f:
+            outputdata = f.read()
+            fileExist = True
 
-	## FILL IN HERE...
+        tcpCliSock.send(b"HTTP/1.0 200 OK\r\n")
+        tcpCliSock.send(b"Content-Type:text/html\r\n\r\n")
+        tcpCliSock.send(outputdata)
 
-	filetouse = ## FILL IN HERE...
+    except IOError:
+        if not fileExist:
+            try:
+                hostn = filename.split('/')[0]
+                c = socket(AF_INET, SOCK_STREAM)
+                c.connect((hostn, 80))
+                c.sendall(message.encode())
 
-	try:
-		# Check wether the file exist in the cache
+                response = b""
+                while True:
+                    data = c.recv(4096)
+                    if not data:
+                        break
+                    response += data
+                c.close()
 
-		## FILL IN HERE...
+                os.makedirs("cache", exist_ok=True)
+                with open(filetouse, "wb") as cache_file:
+                    cache_file.write(response)
 
-		fileExist = "true"
-		# ProxyServer finds a cache hit and generates a response message
-		tcpCliSock.send("HTTP/1.0 200 OK\r\n")            
-		tcpCliSock.send("Content-Type:text/html\r\n")
+                tcpCliSock.sendall(response)
+            except Exception as e:
+                print("Illegal request", e)
+        else:
+            tcpCliSock.send(b"HTTP/1.0 404 Not Found\r\n")
+            tcpCliSock.send(b"Content-Type:text/html\r\n\r\n")
 
-
-		## FILL IN HERE...
-
-
-	# Error handling for file not found in cache, need to talk to origin server and get the file
-	except IOError:
-		if fileExist == "false": 
-
-			## FILL IN HERE...
-			except:
-				print("Illegal request")                                               
-		else:
-			# HTTP response message for file not found
-			tcpCliSock.send("HTTP/1.0 404 sendErrorErrorError\r\n")                             
-			tcpCliSock.send("Content-Type:text/html\r\n")
-			tcpCliSock.send("\r\n")
-
-	# Close the client and the server sockets    
-	tcpCliSock.close() 
+    tcpCliSock.close()
 tcpSerSock.close()
